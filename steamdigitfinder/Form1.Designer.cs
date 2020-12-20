de42f113abc5685d4502f56d8a54af4de7685909
reaksiyon1337 <37051115+reaksiyon@users.noreply.github.com>
2020-12-20 19:52:02 +0300

    init

---
+++ steamdigitfinder/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace steamdigitfinder
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(28, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 59);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "SEARCH";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(23, 130);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(280, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Waiting for an action..";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(175, 25);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 59);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "CONTINUE";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "4digit",
+            "5digit"});
+            this.comboBox1.Location = new System.Drawing.Point(28, 90);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 226);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(268, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Note: Program can be freeze when searching, just wait!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(572, 248);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Steam Untaken ID Finder";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ steamdigitfinder/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using HtmlAgilityPack;
+
+namespace steamdigitfinder
+{
+    public partial class Form1 : Form
+    {
+        int sayac = 1000;
+        
+        string gelenbilgiler = "";
+        public Form1()
+        {
+            
+            InitializeComponent();
+            comboBox1.Text = "4digit";
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            initSayac();
+            search();
+            
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            gelenbilgiler = "";
+            sayac++;
+            search();
+        }
+
+        public void initSayac()
+        {
+            if(comboBox1.Text == "4digit")
+            {
+                sayac = 1000;
+            }else
+            {
+                sayac = 10000;
+            }
+
+
+        }
+        public void search()
+        {
+            
+            while (gelenbilgiler != "Steam Community :: Error")
+            {
+                try { 
+                string hedef = "https://steamcommunity.com/id/" + sayac;
+                WebRequest istek = HttpWebRequest.Create(hedef);
+                WebResponse yanıt;
+                yanıt = istek.GetResponse();
+                StreamReader bilgiler = new StreamReader(yanıt.GetResponseStream());
+                string gelen = bilgiler.ReadToEnd();
+                int baslangic = gelen.IndexOf("<title>") + 7;
+                int bitis = gelen.Substring(baslangic).IndexOf("</title>");
+                gelenbilgiler = gelen.Substring(baslangic, bitis);
+                label1.Text = ("SEARCHING:" + sayac);
+                sayac++;
+                System.Threading.Thread.Sleep(1);
+                }
+                catch(Exception Ex)
+                {
+
+
+                }
+            }
+
+            label1.Text = ("FOUND!:" + (sayac-1));
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
